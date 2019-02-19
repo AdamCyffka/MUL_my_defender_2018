c3ff08948be7343fdee622e286f5faf3be66cb94
Valentin Masson <valipss@localhost.localdomain>
2019-02-19 17:16:39 +0100

    [+] structures added in .h

--- sources/my_defender.c
+++ sources/my_defender.c
@@ -7,7 +7,24 @@
 
 #include "defender.h"
 
+game_object_t new_object(const char *path_to_spsheet,
+            sfVector2f pos, sfIntRect rect, float speed)
+{
+    game_object_t obj;
+    obj.state = running;
+    obj.speed = speed;
+    obj.sprite = sfSprite_create();
+    obj.texture = sfTexture_createFromFile(path_to_spsheet, NULL);
+    obj.position = pos;
+    obj.jumping_position = pos;
+    obj.rect = rect;
+    sfSprite_setTexture(obj.sprite, obj.texture, sfTrue);
+    sfSprite_setTextureRect(obj.sprite, obj.rect);
+    return (obj);
+}
+
 int my_defender(void)
 {
+    game_object_t *obj = malloc(sizeof(game_object_t) * 18);
     return (0);
 }
